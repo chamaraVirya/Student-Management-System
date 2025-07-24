7a7688bcb7eb1d917f99770a6dbabd9204ed7706
chamaraf <developandtestanddeploy@gmail.com>
2025-07-24 22:09:50 +0100

    Student object created and validation done in the Student class

--- src/main/java/com/student/Main.java
+++ src/main/java/com/student/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("***************STUDENT MANAGEMENT SYSTEM*************");
         System.out.println("***************WELCOME!*************");
+
+        Student s1 = new Student("Kajal", 22, "S-1");
+        s1.enrollCourse("Java");
+        s1.enrollCourse("DSA");
+        s1.enrollCourse("Devops");
+        s1.enrollCourse("C#");
+        //System.out.println(s1);
+        s1.printStudentInfo();
     }
 }
